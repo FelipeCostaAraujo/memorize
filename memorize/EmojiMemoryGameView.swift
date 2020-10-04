e3e537ffa1b21f548ff6b90dd908cfceda18eb9d
FelipeAraujo <felipe.s-04@hotmail.com>
2020-10-04 04:11:57 -0300

    lecture 5,6

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  memorize
 //
 //  Created by Felipe Araujo on 01/08/20.
@@ -9,39 +9,79 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    var viewModel:EmojiMemoryGame
+    @ObservedObject var viewModel:EmojiMemoryGame
     
     var body: some View {
-        HStack{
-            ForEach(viewModel.cards){ card in
+        VStack {
+            Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture{
-                    self.viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 0.75)){
+                        self.viewModel.choose(card: card)
+                    }
                     
                 }
+                .padding(5)
             }
+            .foregroundColor(Color.orange).padding()
+            Button(action: {
+                withAnimation(.easeInOut){
+                    self.viewModel.resetGame()
+                }
+            }, label: { Text("New game") })
         }
-        .foregroundColor(Color.orange).padding().font(Font.largeTitle)
-}
-
-struct CardView:View{
-    var card: MemoryGame<String>.Card
+    }
     
-    var body: some View{
-        ZStack {
-            if card.isFaceUp{
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth:3)
-                Text(card.content)
-            }else{
-                RoundedRectangle(cornerRadius: 10.0).fill()
+    struct CardView:View{
+        var card: MemoryGame<String>.Card
+        
+        var body: some View{
+            GeometryReader { geometry in
+                self.body(for: geometry.size)
             }
         }
+        
+        @State private var animatedBonusRemaining: Double = 0
+        
+        private func startBonusTimeAnimation(){
+            animatedBonusRemaining = card.bonusRemaining
+            withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                animatedBonusRemaining = 0
+            }
+        }
+        
+        @ViewBuilder
+        private func body(for size:CGSize) -> some View{
+            if card.isFaceUp || !card.isMatched {
+                ZStack {
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90),clockwise: true)
+                                .onAppear{
+                                    self.startBonusTimeAnimation()
+                                }
+                        } else {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90),clockwise: true)
+                        }
+                    }
+                    .padding(5)
+                    .opacity(0.4)
+                    Text(card.content)
+                        .font(Font.system(size: fontSize(for: size)))
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                        .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
+                }.cardify(isFaceUp: card.isFaceUp)
+                .transition(AnyTransition.scale)
+            }
+        }
+        
+        private func fontSize(for size: CGSize) -> CGFloat {
+            min(size.width, size.height) * 0.7
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        }
     }
-}
 }
