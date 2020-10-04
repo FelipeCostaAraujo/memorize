046fc168b3a68735b571de5be78eaf9bb0704ab2
FelipeAraujo <felipe.s-04@hotmail.com>
2020-10-04 04:09:05 -0300

    lecture 5, 6

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2020 Felipe Araujo. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    //private(set) usado para ver o parametro da classe por outras classes
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    private static func createMemoryGame() -> MemoryGame<String>{
+        let emojis = ["👻","🎃","🕷","😀","🤨","🥰","😡","🤠","👨🏻‍💻"]
+        //let randomInt = Int.random(in: 5..<6) Homework  - feito
+        
+        return MemoryGame<String>(numberOfPairOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the model
+    
+    var cards: Array<MemoryGame<String>.Card>{
+        model.cards
+    }
+    
+    // MARK: -Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card){
+        //objectWillChange.send()
+        model.choose(card: card)
+    }
+    
+    func resetGame(){
+        model = EmojiMemoryGame.createMemoryGame()
+    }
+}
--- memorize/Grid.swift
+++ memorize/Grid.swift
@@ -8,14 +8,35 @@
 
 import SwiftUI
 
-struct Grid: View {
+struct Grid<Item, ItemView>: View where Item: Identifiable,ItemView: View{
+    var items: [Item]
+    var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem:@escaping (Item) -> ItemView){
+        self.items = items
+        self.viewForItem = viewForItem
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
     }
-}
-
-struct Grid_Previews: PreviewProvider {
-    static var previews: some View {
-        Grid()
+    
+    func body(for layout:GridLayout) -> some View{
+        ForEach(items) {
+            item in self.body(for: item, in: layout)
+        }
     }
+    
+    func body(for item:Item,in layout:GridLayout) -> some View{
+        let index = items.firstIndex(matching: item)
+        if index != nil{
+            
+        }
+        return viewForItem(item)
+            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+            .position(layout.location(ofItemAt: index!))
+    }
+    
+    
 }
